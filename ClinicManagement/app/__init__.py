d636a7b26d7f9d9f869ca1d4a6098490306c6dc0
HadenHoo-web <140795485+HadenHoo-web@users.noreply.github.com>
2024-12-19 23:46:10 +0700

    upgrade

--- ClinicManagement/app/__init__.py
+++ ClinicManagement/app/__init__.py
@@ -6,7 +6,7 @@ import cloudinary
 
 app = Flask(__name__)
 app.secret_key = 'HJGFGHF^&%^&&*^&*YUGHJGHJF^%&YYHB'
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/test?charset=utf8mb4" % quote('1234')
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:%s@localhost/test?charset=utf8mb4" % quote('root')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config["PAGE_SIZE"] = 6
 app.config['CART_KEY'] = 'cart'
